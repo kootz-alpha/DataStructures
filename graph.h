5491a6b6c28d5c0c6d8055f044cfba63037cae2b
kootz-alpha <101920127+kootz-alpha@users.noreply.github.com>
2022-12-29 20:40:41 +0530

    Update graph.h

--- graph.h
+++ graph.h
@@ -5,17 +5,19 @@
 #include<stack>
 #include<algorithm>
 #include<numeric>
+#include<climits>
 
-// Follow 1-based indexing or numbering for the nodes
+// Follow 1-based indexing or numbering for the nodes. Currently not designed for multi-edges or loops or negative weights
 
 using namespace std;
 
 class Graph {
 
-    int numNodes = -1, numEdges = -1, directed = 0, weighted = 0;  // 0 = undirected (unweighted), 1 = directed (weighted). -1 means they aren't yet defined
+    int numNodes = -1, numEdges = -1;  
+    bool directed = false, weighted = false;
 
-
-    // just a utility function for the function minDistance
+   
+    // utility for minDistance()
     int minNode(vector<int> unchecked, vector<int> result) { 
         int min = INT_MAX;
         bool p = true;
@@ -31,40 +33,157 @@ class Graph {
         
         return min;    
     }
-
-    public :
-
-        vector<set<int>> adjList;
-        vector<vector<int>> adjMatrix;    // -1 if there's no edge
-
-
-        Graph(int numN, int numE, vector<vector<int>> edgeList) { // creates an adjacency list
+    
+    // utility for containCycle()
+    bool containCycle(int node, vector<int>& visited) {
             
-            numNodes = numN;
-            numEdges = numE;
-            adjList.resize(numNodes);
-
-            for (auto k : edgeList) {
+        visited[node-1] = 1;
+        
+        for (auto neighbour : adjList[node-1]) {
+            
+            if (visited[neighbour[0]-1] == 0) {
                 
-                int x = k[0], y = k[1];
-                adjList[x-1].insert(y);
-                adjList[y-1].insert(x);
+                if (containCycle(neighbour[0], visited))
+                    return true;
             }
+            else if (visited[neighbour[0]-1] == 1)
+                return true;
+        }
+        visited[node-1] = 2;
+        
+        return false;
+    }
+
+    // utility for containCycle()
+    bool containCycle(int parent, int current, vector<int>& visited) { 
+
+        visited[current-1] = 1;
+
+        for (auto neighbour : adjList[current-1]) {
+
+            if (visited[neighbour[0]-1] == 0) {
+
+                if (containCycle(current, neighbour[0], visited))
+                    return true;
+            }
+            else if (neighbour[0] != parent)
+                return true;
         }
 
-        Graph(int numN, int numE, vector<vector<int>> edgeList, int w) { // pass any random integer as 'w'. This one creates an adjacency matrix
+        return false;
+    }
+        
+        
+    public :
+
+        vector<vector<vector<int>>> adjList;
+        vector<vector<int>> adjMatrix;    
+
+
+        Graph(int numN, vector<vector<int>> edgeList, int listOrMatrix = 0, bool d = false) { 
+            
+            // listOrMatrix = 0, creates an adjacency list. Otherwise creates an adjacency matrix
+            // d = true creates a directed graph
             
             numNodes = numN;
-            numEdges = numE;
-            adjMatrix.resize(numNodes, vector<int>(numNodes, -1));
-            weighted = 1;
-
-            for (auto k : edgeList) {
+            numEdges = edgeList.size();
+            
+            if (edgeList[0].size() == 3) 
+                weighted = true;
                 
-                int x = k[0], y = k[1], c = k[2];
-                adjMatrix[x-1][y-1] = c;
-                adjMatrix[y-1][x-1] = c;
+            directed = d;
+            
+            if (listOrMatrix == 0) {
+                
+                adjList.resize(numNodes);
+                
+                if (weighted) {
+                    
+                    if (!directed) {
+    
+                        for (auto k : edgeList) {
+                        
+                            int x = k[0], y = k[1], c = k[2];
+                            adjList[x-1].push_back({y, c});
+                            adjList[y-1].push_back({x, c});
+                        }
+                    }
+                    else {
+    
+                        for (auto k : edgeList) {
+                        
+                            int x = k[0], y = k[1], c =k[2];
+                            adjList[x-1].push_back({y, c});;
+                        }
+                    }
+                }
+                else {
+
+                    if (!directed) {
+        
+                        for (auto k : edgeList) {
+                        
+                            int x = k[0], y = k[1];
+                            adjList[x-1].push_back({y, 1});
+                            adjList[y-1].push_back({x, 1});
+                        }
+                    }
+                    else {
+        
+                        for (auto k : edgeList) {
+                        
+                            int x = k[0], y = k[1];
+                            adjList[x-1].push_back({y, 1});
+                        }
+                    }
+                }
             }
+            else {
+                
+                adjMatrix.resize(numNodes, vector<int>(numNodes, -1));
+                
+                if (weighted) {
+                    
+                    if (!directed) {
+
+                        for (auto k : edgeList) {
+                    
+                            int x = k[0], y = k[1], c = k[2];
+                            adjMatrix[x-1][y-1] = c;
+                            adjMatrix[y-1][x-1] = c;
+                        }
+                    }
+                    else {
+    
+                        for (auto k : edgeList) {
+                    
+                            int x = k[0], y = k[1], c = k[2];
+                            adjMatrix[x-1][y-1] = c;
+                        }
+                    }
+                }
+                else {
+                    
+                    if (!directed) {
+
+                        for (auto k : edgeList) {
+                    
+                            int x = k[0], y = k[1], c = 1;
+                            adjMatrix[x-1][y-1] = c;
+                            adjMatrix[y-1][x-1] = c;
+                        }
+                    }
+                    else {
+                        
+                        for (auto k : edgeList) {
+                    
+                            int x = k[0], y = k[1], c = 1;
+                            adjMatrix[x-1][y-1] = c;
+                        }
+                    }
+                }
+            }
+            
         }
 
         vector<int> minDistance(int source) { // returns min distance from source to all other nodes. graph can be weighted or unweighted
@@ -216,45 +335,36 @@ class Graph {
             
             return result;     
         }
-    
-        bool containCycle() { // initially pass parent and current as same
+        
+        bool containCycle() { 
 
-                cout<<"inside"<<endl;
-
-                vector<bool> visited(numNodes, false);
-
+            vector<int> visited(numNodes, 0);
+            
+            if (directed) {
+                
+                for (int i = 0; i < numNodes; i++) {
+                    
+                    if (visited[i] == 0) {
+                        
+                        if (containCycle(i+1, visited))
+                            return true;
+                    }
+                }
+            }
+            else {
+                
                 for (int i = 0; i < numNodes; i++) {
 
-                    if (!visited[i]) {
-
+                    if (visited[i] == 0) {
+    
                         if (containCycle(-1, i+1, visited))
                             return true;
                     }
                 }
-
-                return false;
+            }
+            return false;
 
         }
-
-        bool containCycle(int parent, int current, vector<bool>& visited) { // initially pass parent and current as same
-
-                visited[current-1] = true;
-
-                for (int neighbour : adjList[current-1]) {
-
-                    if (!visited[neighbour-1]) {
-
-                        if (containCycle(current, neighbour, visited))
-                            return true;
-                    }
-                    else if (neighbour != parent)
-                        return true;
-                }
-
-                return false;
-        }
-
-
 };
 
 int main() {
@@ -262,9 +372,15 @@ int main() {
     vector<vector<int>> edges = {{1,2, 11}, {1,3, 9}, {2,3, 3}, {3,4, 2}, {2, 4, 1}};
     int n = 4, e = 4;
 
-    Graph a = Graph(n, e, edges, 2); // weighted
+    // will automatically detect whether weighted or not
     
-    Graph b = Graph(n, e, edges); // unweighted
+    Graph a = Graph(n, edges); // undirected, adjacency list
+    
+    Graph b = Graph(n, edges, 0, 1); // directed  adjacency list
+    
+    Graph a1 = Graph(n, edges, 1); // undirected adjacency matrix
+    
+    Graph b1 = Graph(n, edges, 1, 1); // directed adjacency matrix
 
     vector<int> c = a.minDistance(1); // from node 1 to all other nodes
 
